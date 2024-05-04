e322c647179ebd7b8427c6e1ce939d41ff07a7b7
Abhijeet1520 <abhijeetkumar12346@gmail.com>
2024-05-05 00:03:03 +1000

    feat: Add hover effect to HeartButton component

--- components/HeartButton.tsx
+++ components/HeartButton.tsx
@@ -1,10 +1,12 @@
 import { firestore, auth, increment } from "../lib/firebase";
 import { useDocument } from "react-firebase-hooks/firestore";
 import { FaHeartBroken, FaRegHeart, FaHeart } from "react-icons/fa";
+import { useState } from "react";
 
 const HeartButton = ({ postRef }) => {
   const heartRef = postRef.collection("hearts").doc(auth.currentUser.uid);
   const [heartDoc] = useDocument(heartRef);
+  const [isHovered, setIsHovered] = useState(false);
 
   const addHeart = async () => {
     const uid = auth.currentUser.uid;
@@ -26,9 +28,13 @@ const HeartButton = ({ postRef }) => {
   };
 
   return heartDoc?.exists ? (
-    <button className="btn-blue" onClick={removeHeart}><FaHeartBroken color="red" className="mr-2"/> Unheart</button>
+    <button className="btn-blue" onClick={removeHeart} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      {isHovered ? <><FaHeartBroken color="red" className="mr-2" /> Unheart</> : <><FaHeart color="red" className="mr-2" /> Heart</>}
+    </button>
   ) : (
-    <button className="btn-blue" onClick={addHeart}><FaHeart color="red" className="mr-2"/>Heart</button>
+    <button className="btn-blue" onClick={addHeart}>
+      <FaRegHeart color="red" className="mr-2" /> Heart
+    </button>
   );
 };
 
