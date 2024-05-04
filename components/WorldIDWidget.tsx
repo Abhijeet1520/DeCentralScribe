f7e478b846453bc0e0c55162e939b355183a0be5
abhijeet1520 <Abhijeetkumar12346@gmail.com>
2024-05-04 11:02:28 +0530

    update variables

--- components/WorldIDWidget.tsx
+++ components/WorldIDWidget.tsx
@@ -42,7 +42,7 @@ export const WorldIdWidget = ({ signal, onProofGenerated }: Props) => {
   // If not yet verified, render the IDKitWidget
   return (
     <IDKitWidget
-      app_id={process.env.NEXT_PUBLIC_WORLD_ID_APP_ID as `app_${string}`}
+      app_id={process.env.NEXT_PUBLIC_WORLD_APP_ID as `app_${string}`}
       action={process.env.NEXT_PUBLIC_WORLD_ID_ACTION_ID}
       signal={signal}
       onSuccess={(proofResult) => {
