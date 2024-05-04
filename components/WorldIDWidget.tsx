a6e05ec691fab2d28a456f6f6837a1011ab28f61
Abhijeet1520 <abhijeetkumar12346@gmail.com>
2024-05-05 08:12:51 +1000

    chore: Add tick icon and verification status to WorldIDWidget

--- components/WorldIDWidget.tsx
+++ components/WorldIDWidget.tsx
@@ -1,6 +1,7 @@
 import { VerificationLevel, IDKitWidget } from "@worldcoin/idkit";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { TiTick } from "react-icons/ti"; // Import the tick icon
 
 // Define Props type
 type Props = {
@@ -18,10 +19,18 @@ export const WorldIdWidget = ({ signal, onProofGenerated }: Props) => {
   const isBrowser = typeof window !== 'undefined';
 
   const [hasEntry, setHasEntry] = useState(false);
+  const [isVerified, setIsVerified] = useState(false); // State to track if verified
 
   if (hasEntry) {
     return <p>You have already verified with World ID</p>;
   }
+  useEffect(() => {
+    // Check if user is already verified by checking localStorage
+    const isVerifiedInLocalStorage = localStorage.getItem("worldIdVerified");
+    if (isVerifiedInLocalStorage) {
+      setIsVerified(true);
+    }
+  }, []);
 
   // If not yet verified, render the IDKitWidget
   return (
@@ -33,17 +42,24 @@ export const WorldIdWidget = ({ signal, onProofGenerated }: Props) => {
         console.log("debug::onSuccess", JSON.stringify(proofResult));
         const { proof, merkle_root, nullifier_hash } = proofResult;
 
+        // Save verification status in localStorage
+        localStorage.setItem("worldIdVerified", "true");
+        localStorage.setItem("worldIdproofResult", JSON.stringify(proofResult));
+        setIsVerified(true); // Update state to reflect verification
+
         toast.success("Successfully verified with World ID");
       }}
       handleVerify={(proof) => {
-        console.log("debug::handleVerify", JSON.stringify(proof));
+        console.log("debug::handleVerify", JSON.stringify(proof), onProofGenerated);
         onProofGenerated && onProofGenerated(proof);
       }}
       verification_level={VerificationLevel.Device}
     >
       {({ open }) => (
-        <button className="border border-black rounded-md" onClick={open}>
-          <div className="mx-3 my-1">Verify with World ID</div>
+        <button className="btn-blue border border-black rounded-md" onClick={open}>
+          <div className="mx-2 my-1">
+            {isVerified ? <span className="flex items-center"><TiTick className="text-green-500 mr-1" /> <span>Verified</span></span> : "Verify with World ID"}
+            </div>
         </button>
       )}
     </IDKitWidget>
